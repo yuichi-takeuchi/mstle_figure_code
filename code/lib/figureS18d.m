56b15a6a95bb70b0fe01a10a88cf8b8df2b81bed
yuichi-takeuchi <yuichi-takeuchi@umin.net>
2020-08-16 20:56:34 +0200

    update figure s17, s18

---
+++ code/lib/figureS18d.m
@@ -0,0 +1,102 @@
+function [sBasicStats, sStatsTest, sBasicStats_MI, sStatsTest_MI, No] = figureS18d()
+% Calcurates and clusters modulation index of HPC electrographic seizures.
+% Copyright (c) 2019, 2020 Yuichi Takeuchi
+
+%% params
+supplement = 'S';
+figureNo = 18;
+panel = 'd';
+inputFileName = ['Figure' supplement num2str(figureNo) '_Fg602_ClosedLoopStim.csv'];
+outputFileName = ['figure' supplement num2str(figureNo) panel '.mat'];
+
+%% Data import
+orgTb = readtable(['../data/' inputFileName]); % original csv data
+supraTb = orgTb(logical(orgTb.Supra),:); % 
+VarNames = orgTb.Properties.VariableNames(15:19); % {RS, WDS, ADDrtn, HPCDrtn, CtxDrtn}
+
+%% Basic statistics and Statistical tests
+% supra
+[ sBasicStats, sStatsTest ] = statsf_getBasicStatsAndTestStructs1( supraTb, VarNames, supraTb.Laser );
+
+%% Calculation of parameters (MI)
+% getting parameters (supra)
+HPCOff = supraTb.HPCDrtn(supraTb.Laser == false);
+HPCOn  = supraTb.HPCDrtn(supraTb.Laser == true);
+supraMI = (HPCOn-HPCOff)./(HPCOn+HPCOff);
+
+%% Skewness test
+supraMIpos = supraMI(supraMI >= 0);
+supraMIneg = supraMI(supraMI <= 0);
+[ sBasicStats_MI, sStatsTest_MI ] = statsf_getBasicStatsAndTestStructs2( supraMIpos, abs(supraMIneg) );
+
+%% Figure preparation of MI with curve fitting with one Gaussian component
+% fitting
+binWidth = 0.1;
+
+numGaussian = 1;
+gmdist = fitgmdist(supraMI, numGaussian);
+gmsigma = gmdist.Sigma;
+gmmu = gmdist.mu;
+gmwt = gmdist.ComponentProportion;
+x = linspace(-1, 1, 1000);
+fitdata = pdf(gmdist, x')*gmwt(1)*binWidth;
+
+% id of animals
+idVec = supraTb.LTR(supraTb.Laser == true);
+edges = [-1:binWidth:1];
+close all
+
+hfig = figure(1);
+hax = axes;
+
+% building a plot
+[ hs ] = figf_BarAsStackedHistWOneFit1(hax, supraMI, idVec, edges, x, fitdata, 'probability' );
+
+% global parameters
+fontname = 'Arial';
+fontsize = 5;
+
+% figure parameter settings
+set(hfig,...
+    'PaperUnits', 'centimeters',...
+    'PaperPosition', [0.5 0.5 4 4],... % [h distance, v distance, width, height], origin: left lower corner
+    'PaperSize', [5 5]... % width, height
+    );
+
+% axis parameter settings
+set(hs.ax,...
+    'FontName', fontname,...
+    'FontSize', fontsize...
+    );
+
+incrStep = 0.8/length(unique(supraTb.LTR));
+for i = 1:length(unique(supraTb.LTR))
+    set(hs.bar(i),...
+        'FaceColor', [0.1+i*incrStep 0.1+i*incrStep 0.1+i*incrStep]...
+        );
+end
+
+% fitting curve parameter settings
+set(hs.plt,...
+    'Color', [0 0 0]...
+    );
+
+set(hs.ylbl, 'String', 'Probability');
+set(hs.xlbl, 'String', 'Modulation index');
+set(hs.ttl, 'String', 'MI of HPC seizures');
+
+% outputs
+print(['../results/figure' supplement num2str(figureNo) panel '.pdf'], '-dpdf');
+print(['../results/figure' supplement num2str(figureNo) panel '.png'], '-dpng');
+
+close all
+
+%% Number of rats and trials
+No.Rats = length(unique(supraTb.LTR));
+No.Trials = length(supraTb.LTR);
+
+%% Save
+save(['../results/' outputFileName], 'sBasicStats', 'sStatsTest', 'sBasicStats_MI', 'sStatsTest_MI', 'No', '-v7.3')
+disp('done')
+
+end
---
+++ code/lib/figureS18e.m
@@ -0,0 +1,101 @@
+function [sBasicStats, sStatsTest, sBasicStats_pa, sStatsTest_pa, No] = figureS18e()
+% Copyright(c) 2018–2020 Yuichi Takeuchi
+
+%% params
+supplement = 'S';
+figureNo = 18;
+panel = 'e';
+inputFileName = ['Figure' supplement num2str(figureNo) '_Fg602_ClosedLoopStim.csv'];
+outputFileName = ['figure' supplement num2str(figureNo) panel '.mat'];
+
+%% Data import 
+orgTb = readtable(['../data/' inputFileName]); % original csv data
+supraTb = orgTb(logical(orgTb.Supra),:); % 
+VarNames = orgTb.Properties.VariableNames([18, 19, 15]); % {HPCDrtn, CtxDrtn, RS}
+
+%% Basic statistics and Statistical tests
+% supra
+[ sBasicStats, sStatsTest ] = statsf_getBasicStatsAndTestStructs1( supraTb, VarNames, supraTb.Laser );
+
+%% animal basis stats (independent)
+for i = 1:length(VarNames)
+    [MeanPerAnimal, ~, intrvntnVec] = statsf_meanPer1With2(supraTb.(VarNames{i}), supraTb.LTR, supraTb.Laser);
+    [sBasicStats_pa(i)] = stats_sBasicStats_anova1( MeanPerAnimal, intrvntnVec );
+    [sStatsTest_pa(i)] = statsf_2sampleTestsStatsStruct_cndtn( supraTb.(VarNames{i}), supraTb.Laser);
+end
+
+%% Figure preparation (clustered)
+% Common labelings
+CTitle = {'HPC electrographic seizure', 'Ctx electrographic seizure', 'Motor seizure'};
+CVLabel = {'Duration (s)', 'Duration (s)', 'Racine''s scale'};
+
+close all
+hfig = figure(1);
+for i = 1:length(VarNames)
+        
+    % figure parameter settings
+    set(hfig,...
+        'PaperUnits', 'centimeters',...
+        'PaperPosition', [0.5 0.5 14 4],... % [h distance, v distance, width, height], origin: left lower corner
+        'PaperSize', [15 5]... % width, height
+        );
+    
+    % global parameters
+    fontname = 'Arial';
+    fontsize = 5;
+    
+    % axis
+    hax = subplot(1, 3, i);
+    
+    % building a plot
+    [ hs ] = figf_BarMeanIndpndPlot1( supraTb.LTR, supraTb.(VarNames{i}), supraTb.Laser + 1, hax );
+ 
+    % setting parametors of bars and plots
+    set(hs.bar,'FaceColor',[1 1 1],'EdgeColor',[0 0 0],'LineWidth', 0.5);
+    
+    for j = 1:size(hs.cplt, 2)
+        set(hs.cplt{j}, 'LineWidth', 0.5, 'MarkerSize', 4); % 'Color', [0 0 0]
+    end
+    
+    set(hs.xlbl, 'String', 'Laser');
+    set(hs.ylbl, 'String', CVLabel{i});
+    set(hs.ttl, 'String', CTitle{i});
+    
+    % patch
+    hax = gca;
+    yl = get(hax, 'YLim');
+    hptch = patch([1.6 2.4 2.4 1.6],[yl(1) yl(1) yl(2) yl(2)],'b');
+    set(hptch,'FaceAlpha',0.2,'edgecolor','none');
+    
+    % axis parameter settings
+    set(hs.ax,...
+        'YLim', yl,...
+        'XLim', [0.5 2.5],...
+        'XTick', [1 2],...
+        'XTickLabel', {'Off', 'On'},...
+        'FontName', fontname,...
+        'FontSize', fontsize...
+        );
+end
+
+% outputs
+print(['../results/figure' supplement num2str(figureNo) panel '.pdf'], '-dpdf');
+print(['../results/figure' supplement num2str(figureNo) panel '.png'], '-dpng');
+
+close all
+
+%% Number of rats and trials
+No.Rats = length(unique(supraTb.LTR));
+No.Trials = length(supraTb.LTR);
+
+%% Save
+save(['../results/' outputFileName],...
+    'sBasicStats',...
+    'sStatsTest',...
+    'sBasicStats_pa',...
+    'sStatsTest_pa',...
+    'No',...
+    '-v7.3')
+disp('done')
+
+end
