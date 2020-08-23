51a874998871bb4965c8082153d9ea3833603cba
yuichi-takeuchi <yuichi-takeuchi@umin.net>
2020-08-23 22:49:15 +0200

    add figureS4_main

---
+++ code/lib/figureS4b.m
@@ -0,0 +1,28 @@
+function figureS4b()
+% Copyright (c) 2017–2020 Yuichi Takeuchi
+
+%% data import
+load('../data/FigureS4_Fg_599_DataSum.mat')
+
+%% Recording list
+RecList = fieldnames(UnitSum);
+for i = 1:length(RecList)
+    disp([num2str(i) ': ' RecList{i}])
+end
+
+%% Unit Info
+recID = 9;
+UnitInfo = UnitSum.(RecList{recID});
+
+%% Unit char automatic
+% This function gives a summary figure of the unit and prints and pdf of
+% the figure
+CluID = 7; % not a record number
+numRec = [UnitInfo.CluID] == CluID;
+close all
+fignum = 1; 
+[ hstruct ] = unf_UnitCharactorization1( UnitInfo, numRec, fignum, 'figureS4b');
+close all
+disp('done')
+
+end
