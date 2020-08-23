13b5cecf18613d9d1c37431db67157f76051f123
yuichi-takeuchi <yuichi-takeuchi@umin.net>
2020-08-23 22:56:28 +0200

    update figure s8

--- code/lib/lfp_PowerAnalysis_epilepsy_figure_1.m
+++ code/lib/lfp_PowerAnalysis_epilepsy_figure_1.m
@@ -5,7 +5,7 @@
 clc
 clear
 %loadcate pwrspctrm first
-load('../data/Powerall.mat')
+load('../data/FigureS8_Powerall.mat')
 %%
 xtick = [1,4,13,30,50,90,150];
 nch = numel(chname);
@@ -52,7 +52,7 @@ for i = 1:nch
     'PaperSize', [10 7],...
     'paperposition', [0 0 10 7],...
     'renderer','painters')
-    print(gcf,'-dpdf',strcat(['../results/ch-' num2str(i) '-' chname{i}]));
+    print(gcf,'-dpdf',strcat(['../results/FigureS8_ch-' num2str(i) '-' chname{i}]));
     close gcf
 end
 disp('finished')
@@ -124,7 +124,7 @@ for i = 1:nch
     'PaperSize', [10 7],...
     'paperposition', [0 0 10 7],...
     'renderer','painters')
-    print(gcf,'-dpdf',strcat(['../results/Band-ch-' num2str(i) '-' chname{i}]));
+    print(gcf,'-dpdf',strcat(['../results/FigureS8_Band-ch-' num2str(i) '-' chname{i}]));
     close gcf
 end
 clear i j X Y1 hfig  hb xf yf1 yf2 xstar star s xlim ylim errhigh
